cacebbb4452012c0292622e9a552acf9ae2e17a9
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-17 11:27:30 +0700

    konversi signup page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,101 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
+    <div class="page-content page-auth" id="register">
+      <div class="section-store-auth" data-aos="fade-up">
+        <div class="container">
+          <div class="row align-items-center row-login justify-content-center">
+            <div class="col-lg-5">
+              <h2>
+                Memulai untuk jual beli <br />
+                dengan cara terbaru
+              </h2>
+              <form action="" class="mt-3">
+                <div class="form-group">
+                  <label>Full Name</label>
+                  <input
+                    type="text"
+                    class="form-control is-valid"
+                    v-model="name"
+                    autofocus
+                  />
+                </div>
+                <div class="form-group">
+                  <label>Email Address</label>
+                  <input
+                    type="text"
+                    class="form-control is-invalid"
+                    v-model="email"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="password">Password</label>
+                  <input
+                    type="text"
+                    name="password"
+                    id="password"
+                    class="form-control"
+                  />
+                </div>
+                <div class="form-group">
+                  <label>Store</label>
+                  <p class="text-muted">Apakah anda juga ingin membuka toko?</p>
+                  <div
+                    class="custom-control custom-radio custom-control-inline"
+                  >
+                    <input
+                      type="radio"
+                      name="is_store_open"
+                      id="openStoreTrue"
+                      class="custom-control-input"
+                      v-model="is_store_open"
+                      :value="true"
+                    />
+                    <label for="openStoreTrue" class="custom-control-label"
+                      >Iya, boleh.</label
+                    >
+                  </div>
+                  <div
+                    class="custom-control custom-radio custom-control-inline"
+                  >
+                    <input
+                      type="radio"
+                      name="is_store_open"
+                      id="openStoreFalse"
+                      class="custom-control-input"
+                      v-model="is_store_open"
+                      :value="false"
+                    />
+                    <label for="openStoreFalse" class="custom-control-label"
+                      >Enggak, makasih.</label
+                    >
+                  </div>
+                </div>
+                <div class="form-group" v-if="is_store_open">
+                  <label>Nama Toko</label>
+                  <input type="text" class="form-control" name="store_name" />
+                </div>
+                <div class="form-group" v-if="is_store_open">
+                  <label for="">Category</label>
+                  <select name="category" class="form-control">
+                    <option disabled selected>Select Category</option>
+                  </select>
+                </div>
+                <a href="/dashboard.html" class="btn btn-success btn-block mt-4"
+                  >Sign Up Now</a
+                >
+                <a href="/login.html" class="btn btn-signup btn-block mt-4"
+                  >Back to Sign In</a
+                >
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    {{-- Bawaan Laravel --}}
+    <div class="container" style="display: none">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -75,3 +169,33 @@
     </div>
 </div>
 @endsection
+
+@push('addon-script')
+    <script src="/vendor/vue/vue.js"></script>
+    <script src="/vendor/vue-toasted/dist/vue-toasted.js"></script>
+    <script>
+      Vue.use(Toasted);
+
+      var register = new Vue({
+        el: "#register",
+        data: {
+          name: "Rosyid Grobogan",
+          email: "rosyid@abc.com",
+          password: "",
+          is_store_open: true,
+          store_name: "",
+        },
+        mounted() {
+          AOS.init();
+          this.$toasted.error(
+            "Maaf, tampaknya email sudah terdaftar pada sistem kami.",
+            {
+              position: "top-center",
+              className: "rounded",
+              duration: 1000,
+            }
+          );
+        },
+      });
+    </script>
+@endpush
---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>@yield('title')</title>
+    <meta name="viewport" content="width=device-width" />
+        {{-- Style --}}
+        @stack('prepend-style')
+        @include('includes.style')
+        @stack('addon-style')
+  </head>
+  <body>
+    {{-- Navigation Bar --}}
+    @include('includes.navbar-auth')
+
+    {{-- Page Content --}}
+    @yield('content')
+
+    {{-- Footer --}}
+    @include('includes.footer')
+
+    {{-- Script --}}
+    @stack('prepend-script')
+    @include('includes.script')
+    @stack('addon-script')
+  </body>
+</html>
