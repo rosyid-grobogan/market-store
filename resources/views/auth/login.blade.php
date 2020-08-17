259c926ffe5b6c808ac1a623404d46ce63d028f8
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-17 11:32:15 +0700

    konversi signin page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,57 @@
-@extends('layouts.app')
-
+@extends('layouts.auth')
+@section('title', 'Sign In Page')
+    
 @section('content')
-<div class="container">
+    <div class="page-content page-auth">
+      <div class="section-store-auth" data-aos="fade-up">
+        <div class="container">
+          <div class="row align-items-center row-login">
+            <div class="col-lg-6 text-center">
+              <img
+                src="/images/signin-holder.jpg"
+                alt=""
+                class="w-50 mb-4 mb-lg-none"
+              />
+            </div>
+            <div class="col-lg-5">
+              <h2>Belanja kebutuhan utama, menjadi lebih mudah</h2>
+              <form action="">
+                <div class="form-group">
+                  <label for="email">Email Address</label>
+                  <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    class="form-control w-75"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="password">Password</label>
+                  <input
+                    type="text"
+                    name="password"
+                    id="password"
+                    class="form-control w-75"
+                  />
+                </div>
+                <a
+                  href="/dashboard.html"
+                  class="btn btn-success btn-block w-75 mt-4"
+                  >Sign In to My Account</a
+                >
+                <a
+                  href="/register.html"
+                  class="btn btn-signup btn-block w-75 mt-4"
+                  >Sign Up</a
+                >
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+{{-- Login Default --}}
+<div class="container" style="display: none">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
