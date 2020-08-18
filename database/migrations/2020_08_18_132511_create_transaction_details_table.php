3ad8a363afe9a7d45668bbf8c762823c8fd7c1b6
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-18 20:33:43 +0700

    migration: transaction_details

---
+++ database/migrations/2020_08_18_132511_create_transaction_details_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_details', function (Blueprint $table) {
+            $table->id();
+            $table->integer('transactions_id');
+            $table->integer('products_id');
+            $table->string('code');
+            $table->integer('price');
+            $table->string('resi');
+            $table->string('shipping_status');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_details');
+    }
+}
