f4c576857b3f1433417ec26fe7a8e69fc00b9eae
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-19 14:32:12 +0700

    admin/categories markup page

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -54,7 +54,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('pages.admin.categories.create');
     }
 
     /**
---
+++ resources/views/pages/admin/categories/create.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts.admin')
+@section('title', 'Categories')
+@section('Categories', 'active')
+@push('addon-style')
+<link rel="stylesheet" type="text/css" href="{{ asset('vendor/DataTables/datatables.min.css') }}"/>
+@endpush
+@section('content')
+          <div
+            class="section-content section-dashboard-home"
+            data-aos="fade-up">
+            <div class="container-fluid">
+              <div class="dashboard-heading">
+                <h2 class="dashboard-title">Admin Categories</h2>
+                <p class="dashboard-subtitle">Create new category</p>
+              </div>
+              <div class="dashboard-content">
+                <div class="row mt-3">
+                    <div class="col-md-12">
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors as $erros)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <div class="card">
+                            <div class="card-body">
+                                <form action="{{ route('categories.store') }}" method="POST" enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>Nama Kategori</label>
+                                                <input type="text" class="form-control" required>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>Icon</label>
+                                                <input type="file" class="form-control" required>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col text-right">
+                                            <button type="submit" class="btn btn-success px-5 btn-lg">
+                                                Create Now
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+@endsection
+
+@push('addon-script')
+    <script>
+      AOS.init();
+      //   Show-Hide Menu
+      $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+      });
+    </script>
+@endpush
