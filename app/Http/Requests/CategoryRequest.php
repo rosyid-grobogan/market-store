352f5ad449b3abc88432ed4746ade2e2bd39a4c4
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-19 14:06:55 +0700

    Form Request for Category

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -13,7 +13,7 @@ class CategoryRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class CategoryRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required|string',
+            'photo' => 'required|image'
         ];
     }
 }
