7b385dc1170e4096a2f4762fa53ca25cab031cb5
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-20 09:15:41 +0700

    Product Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Product extends Model
 {
     use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'users_id',
+        'categories_id',
+        'price',
+        'description',
+        'slug'
+    ];
+
+    protected $hidden = [
+
+    ];
 }
