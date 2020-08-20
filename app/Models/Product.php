49127d25cfe0890b9dd7324514b9c4cdff3ac14b
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-20 09:26:24 +0700

    Product Model: relationship

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -21,4 +21,19 @@ class Product extends Model
     protected $hidden = [
 
     ];
+
+    public function galleries()
+    {
+        return $this->hasMany(ProductGallery::class, 'products_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'users_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'categories_id', 'id');
+    }
 }
