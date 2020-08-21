f6d9777dae01984b5cfe3ad2ccdc671541a5ea6a
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-21 19:31:52 +0700

    Fixing Login Page

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -24,17 +24,72 @@
             <li class="nav-item">
               <a href="/rewards.html" class="nav-link">Rewards</a>
             </li>
+
+            @guest
             <li class="nav-item">
-              <a href="/register.html" class="nav-link">Sign Up</a>
+              <a href="{{ route('register') }}" class="nav-link">Sign Up</a>
             </li>
             <li class="nav-item">
               <a
-                href="/login.html"
+                href="{{ route('login') }}"
                 class="btn btn-success nav-link px-4 text-white nav-link"
                 >Sign In</a
               >
             </li>
+            @endguest
+
+
           </ul>
+          @auth
+                <!-- Desktop Menu -->
+                <ul class="navbar-nav d-none d-lg-flex">
+                  <li class="nav-item dropdown">
+                    <a
+                      href="#"
+                      class="nav-link"
+                      id="navbarDropdown"
+                      role="button"
+                      data-toggle="dropdown"
+                    >
+                      <img
+                        src="/images/user-pc.png"
+                        alt=""
+                        class="rounded-circle mr-2 profile-pricture"
+                      />
+                      Hi, {{ Auth::User()->name }}
+                    </a>
+                    <div class="dropdown-menu">
+                      <a href="/dashboard.html" class="dropdown-item"
+                        >Dashboard</a
+                      >
+                      <a href="/dashboard-account.html" class="dropdown-item"
+                        >Setting</a
+                      >
+                      <div class="dropdown-divider"></div>
+                      <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="dropdown-item">Logout</a>
+                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none">
+                        @csrf
+                    </form>
+                    </div>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#" class="nav-link d-inline-block mt-2">
+                      <img src="/images/cart2.svg" alt="" />
+                      <div class="card-badge">3</div>
+                    </a>
+                  </li>
+                </ul>
+
+                <!-- Mobile -->
+                <ul class="navbar-nav d-block d-lg-none">
+                  <li class="nav-item">
+                    <a href="#" class="nav-link">Hi, {{ Auth::User()->name }}</a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#" class="nav-link d-inline-block">Cart</a>
+                  </li>
+                </ul>
+          @endauth
         </div>
       </div>
     </nav>
