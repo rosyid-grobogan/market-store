4fec9c1f3704eca3ab39821f4abc01f821db6180
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-23 13:45:27 +0700

    Api Location

--- app/Http/Controllers/Api/LocationController.php
+++ app/Http/Controllers/Api/LocationController.php
@@ -4,8 +4,18 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Province;
+use App\Models\Regency;
 
 class LocationController extends Controller
 {
-    //
+    public function provinces(Request $request)
+    {
+        return Province::all();
+    }
+
+    public function regencies(Request $request, $provinces_id)
+    {
+        return Regency::where('province_id', $provinces_id)->get();
+    }
 }
