efe9eb97c1b7c33102cf6c0e9a70b9ffc5abe2ce
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-24 10:04:24 +0700

    Model: Transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = [
+        'users_id',
+        'inscurance_price',
+        'shipping_price',
+        'total_price',
+        'code',
+    ];
 }
