5e15f077931048c2443b5063a9ca9628b0f07bd4
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-24 10:14:53 +0700

    Model:Update User

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,13 @@ class User extends Authenticatable
         'store_name',
         'categories_id',
         'store_status',
+        'address_one',
+        'address_two',
+        'provinces_id',
+        'regencies_id',
+        'zip_code',
+        'country',
+        'phone_number'
     ];
 
     /**
