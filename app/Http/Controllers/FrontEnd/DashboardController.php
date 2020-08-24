a1618d8f479b4f02785d3223e6ac629e7e5d9ead
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-25 05:33:41 +0700

    DashboardController: index

--- app/Http/Controllers/FrontEnd/DashboardController.php
+++ app/Http/Controllers/FrontEnd/DashboardController.php
@@ -4,12 +4,29 @@ namespace App\Http\Controllers\FrontEnd;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use App\Models\TransactionDetail;
+use App\Models\User;
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('pages.dashboard.index');
+        $transactions = TransactionDetail::with(['transaction.user','product.galleries'])
+                                        ->whereHas('product', function($product) {
+                                            $product->where('users_id', Auth::user()->id);
+                                        });
+
+        $revenue = $transactions->get()->reduce(function ($carry, $item) {
+            return $carry + $item->price;
+        });
+        $customer = User::count();
+
+        return view('pages.dashboard.index', [
+            'transaction_count' => $transactions->count(),
+            'transaction_data' => $transactions->get(),
+            'revevue' => $revenue,
+            'customer' => $customer
+        ]);
     }
 
     public function products()
