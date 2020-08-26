fca2c7aded81d7296b3d4daf828acd4e636f4004
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-26 12:09:27 +0700

    Dashboard: Transaction index

--- app/Http/Controllers/Dashboard/TransactionController.php
+++ app/Http/Controllers/Dashboard/TransactionController.php
@@ -3,9 +3,32 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\TransactionDetail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TransactionController extends Controller
 {
-    //
+    public function index()
+    {
+        $sellTransactions = TransactionDetail::with(['transaction.user', 'product.galleries'])
+                                            ->whereHas('product', function($product) {
+                                                $product->where('users_id', Auth::user()->id);
+                                            })
+                                            ->get();
+        $buyTransactions = TransactionDetail::with(['transaction.user', 'product.galleries'])
+                                            ->whereHas('transaction', function($transaction) {
+                                                $transaction->where('users_id', Auth::user()->id);
+                                            })
+                                            ->get();
+        return view('pages.dashboard.transactions.index', [
+            'sellTransactions' => $sellTransactions,
+            'buyTransactions' => $buyTransactions,
+        ]);
+    }
+
+    public function show()
+    {
+        return view('pages.dashboard.transactions.details');
+    }
 }
